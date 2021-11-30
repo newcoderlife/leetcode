e43c30c5365f95c2312b358a7560b7ec23468da9
newcoderlife <newcoderlife@gmail.com>
2021-11-30 23:21:09 +0800

    getIntersectionNode

---
+++ go/get_intersection_node/base.go
@@ -0,0 +1,27 @@
+package get_intersection_node
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	p1, p2 := headA, headB
+	for p1 != p2 {
+		if p1 == nil {
+			p1 = headB
+		} else {
+			p1 = p1.Next
+		}
+		if p2 == nil {
+			p2 = headA
+		} else {
+			p2 = p2.Next
+		}
+	}
+	return p1
+}
