9f1a1065ab7838c1bc0df6a29b322f537d65bc19
newcoderlife <newcoderlife@outlook.com>
2021-12-01 23:06:25 +0800

    rob

--- go/rob/base.go
+++ go/rob/base.go
@@ -1,10 +1,5 @@
 package rob
 
-var (
-	memory     []int
-	candidates []int
-)
-
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -12,33 +7,21 @@ func max(a, b int) int {
 	return b
 }
 
-func dfs(current int) int {
-	if current >= len(candidates) {
-		return 0
-	}
-	if memory[current] != -1 {
-		return memory[current]
-	}
-
-	memory[current] = max(candidates[current]+dfs(current+2), candidates[current+1]+dfs(current+3))
-	return memory[current]
-}
-
 func rob(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
-	candidates = nums
-	memory = make([]int, len(nums))
-	for i := 0; i < len(nums); i++ {
-		memory[i] = -1
-	}
-	if len(nums) > 0 {
-		memory[len(nums)-1] = nums[len(nums)-1]
-	}
-	if len(nums) > 1 {
-		memory[len(nums)-2] = max(nums[len(nums)-1], nums[len(nums)-2])
+	if len(nums) == 1 {
+		return nums[0]
 	}
 
-	return dfs(0)
+
+	prev2 := nums[0]
+	prev1 := max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		current := max(prev1, prev2+nums[i])
+		prev2 = prev1
+		prev1 = current
+	}
+	return prev1
 }
