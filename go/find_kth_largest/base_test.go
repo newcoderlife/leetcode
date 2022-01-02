4fa59a7be2f8da9340a7c75f36b2e4fa7242592b
newcoderlife <newcoderlife@gmail.com>
2022-01-02 15:57:57 +0800

    findKthLargest

---
+++ go/find_kth_largest/base_test.go
@@ -0,0 +1,11 @@
+package find_kth_largest
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_findKthLargest(t *testing.T) {
+	// fmt.Println(findKthLargest([]int{3, 2, 1, 5, 6, 4}, 2))
+	fmt.Println(findKthLargest([]int{2, 1}, 2))
+}
