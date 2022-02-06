33d601b564e63271c7fa95d64d75a766968a8ed7
newcoderlife <newcoderlife@gmail.com>
2022-02-06 13:14:05 +0800

    rob

---
+++ go/rob/base.go
@@ -0,0 +1,36 @@
+package rob
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+var (
+	selectHash map[*TreeNode]int
+	passHash   map[*TreeNode]int
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func dfs(current *TreeNode) {
+	if current == nil {
+		return
+	}
+
+	dfs(current.Left)
+	dfs(current.Right)
+
+	selectHash[current] = current.Val + passHash[current.Left] + passHash[current.Right]
+	passHash[current] = max(selectHash[current.Left], passHash[current.Left]) + max(selectHash[current.Right], passHash[current.Right])
+}
+
+func rob(root *TreeNode) int {
+	selectHash, passHash = make(map[*TreeNode]int), make(map[*TreeNode]int)
+	dfs(root)
+	return max(selectHash[root], passHash[root])
+}
