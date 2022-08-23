b276ab214f51381ce909cf13c17efdbf9ae3a333
newcoderlife <newcoderlife@gmail.com>
2022-08-23 23:29:15 +0800

    feat: Finish leetcode 113

--- go/path_sum/base.go
+++ go/path_sum/base.go
@@ -1,19 +1,50 @@
 package path_sum
 
+import (
+	"container/list"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func hasPathSum(root *TreeNode, targetSum int) bool {
+var (
+	buffer  *list.List
+	results [][]int
+)
+
+func hasPathSum(root *TreeNode, targetSum int, buffer *list.List) {
 	if root == nil {
-		return false
+		return
 	}
+
+	buffer.PushBack(root.Val)
+	targetSum -= root.Val
 
 	if root.Left == nil && root.Right == nil {
-		return root.Val == targetSum
+		if targetSum == 0 {
+			result := make([]int, 0, buffer.Len())
+			for current := buffer.Front(); current != nil; current = current.Next() {
+				result = append(result, current.Value.(int))
+			}
+			results = append(results, result)
+		}
+
+		buffer.Remove(buffer.Back())
+		return
 	}
 
-	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+	hasPathSum(root.Left, targetSum, buffer)
+	hasPathSum(root.Right, targetSum, buffer)
+
+	buffer.Remove(buffer.Back())
+}
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	buffer = list.New()
+	results = make([][]int, 0)
+	hasPathSum(root, targetSum, buffer)
+	return results
 }
