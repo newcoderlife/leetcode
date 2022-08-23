e5a1d5b9300c3e81cbb1946eee29f215b0ba4694
newcoderlife <newcoderlife@gmail.com>
2022-08-23 23:11:30 +0800

    feat: Finish leetcode 112

---
+++ go/path_sum/base.go
@@ -0,0 +1,19 @@
+package path_sum
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return root.Val == targetSum
+	}
+
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
