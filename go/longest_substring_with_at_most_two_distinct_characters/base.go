b5118dade924a150ce91d29f3f870da3eb2d5e77
newcoderlife <newcoderlife@gmail.com>
2022-08-26 21:30:19 +0800

    feat: Finish leetcode 159

---
+++ go/longest_substring_with_at_most_two_distinct_characters/base.go
@@ -0,0 +1,26 @@
+package longest_substring_with_at_most_two_distinct_characters
+
+func lengthOfLongestSubstringTwoDistinct(s string) int {
+	exist := make(map[byte]int)
+	prev, result := 0, 0
+	for i := 0; i < len(s); i++ {
+		exist[s[i]]++
+		if exist[s[i]] == 1 && len(exist) > 2 {
+			for {
+				exist[s[prev]]--
+				if exist[s[prev]] == 0 {
+					delete(exist, s[prev])
+					prev++
+					break
+				}
+				prev++
+			}
+		}
+
+		if i-prev+1 > result {
+			result = i - prev + 1
+		}
+	}
+
+	return result
+}
